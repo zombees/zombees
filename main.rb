dcf652f900fcaaae4bef46b5fdfbd63f19f51317
Solomon White <rubysolo@gmail.com>
2013-04-16 07:25:19 -0600

    initial test

--- main.rb
+++ main.rb
@@ -1,16 +1,19 @@
 require 'fog'
 require 'yaml'
 
-aws_config = YAML.load_file('aws_config.yml')
-p aws_config
-connection = Fog::Compute.new(aws_config)
+module AWSConfig
+  def self.run
+    aws_config = YAML.load_file('aws_config.yml')
+    connection = Fog::Compute.new(aws_config)
 
-server = connection.servers.bootstrap(
-  private_key_path:  'tourfleet',
-  public_key_path:   'tourfleet.pub',
-  username:          'ubuntu'
-)
+    server = connection.servers.bootstrap(
+      private_key_path:  'tourfleet',
+      public_key_path:   'tourfleet.pub',
+      username:          'ubuntu'
+    )
 
-server.wait_for { ready? }
-p server.ssh('ls')
+    server.wait_for { ready? }
+    server.ssh('ls')
+  end
 
+end
