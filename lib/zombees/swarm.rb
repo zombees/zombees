427d6507d0eaa8a8f64c295992754c29c549319c
Solomon White <rubysolo@gmail.com>
2013-05-03 07:14:24 -0600

    Added swarm abstraction

---
+++ lib/zombees/swarm.rb
@@ -0,0 +1,31 @@
+require 'celluloid'
+require 'celluloid/pmap'
+
+module Zombees
+  class Swarm
+    attr_reader :worker_count, :command, :worker
+
+    def initialize(options)
+      @worker_count = options[:worker_count]
+      @command = options[:command]
+      @worker = options[:worker]
+      @population = options[:population]
+    end
+
+    def breed
+      @population ||= worker_count.downto(1).pmap do
+        worker.bootstrap
+      end
+    end
+
+    def population
+      breed
+    end
+
+    def run
+      population.pmap do |server|
+        server.run_command(@command)
+      end
+    end
+  end
+end
---
+++ spec/zombees/swarm_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'zombees/swarm'
+
+module Zombees
+  describe Swarm do
+    let(:server) { mock('Server') }
+    let(:worker) { mock('Worker', bootstrap: server) }
+
+    it 'bootstraps requested number of servers' do
+      swarm = described_class.new(worker_count: 3, command: "ls", worker: worker)
+      worker.should_receive(:bootstrap).exactly(3).times.and_return(stub(run_command: true))
+
+      swarm.breed
+    end
+
+    it 'distributes a command to the population of workers' do
+      servers = (1..3).map do |i| 
+        mock("Server#{i}").tap { |s| s.should_receive(:run_command).once.with("ls") }
+      end
+      swarm = described_class.new(command: "ls", population: servers)
+      swarm.run
+    end
+  end
+end
