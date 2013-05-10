f874f06a3a5c9951f71c1b7d0aeb06a6da381df9
Solomon White <rubysolo@gmail.com>
2013-05-10 07:49:50 -0600

    Ab adapter spec for the command to run

--- lib/zombees/ab_adapter.rb
+++ lib/zombees/ab_adapter.rb
@@ -1,8 +1,27 @@
 module Zombees
   module AbAdapter
     class Command
+      attr_reader :requests, :concurrency, :url, :ab_options
+
+      def initialize(options={})
+        options = default.merge(options)
+        @requests    = options[:requests]
+        @concurrency = options[:concurrency]
+        @url         = options[:url]
+        @ab_options  = options[:ab_options]
+      end
+
+      def default
+        {}
+      end
+
       def run(worker)
         worker.run_command(command)
+      end
+
+      def command
+        "ab -r -n #{requests} -c #{concurrency} " +
+        %Q{-C "sessionid=fake" #{ab_options} "#{url}"}
       end
     end
     class Aggregator
--- spec/zombees/ab_adapter_spec.rb
+++ spec/zombees/ab_adapter_spec.rb
@@ -2,13 +2,31 @@ require 'spec_helper'
 require 'zombees/ab_adapter'
 
 module Zombees
-  describe AbAdapter do
-    it 'executes a command on a worker' do
-      adapter = described_class.new
-      adapter.stub(:command).and_return('ab')
-      worker = mock('Worker')
-      worker.should_receive(:run_command).with('ab')
-      adapter.run(worker)
+  module AbAdapter
+    describe Command do
+      context 'command generation' do
+        subject { described_class.new(requests: 1000, concurrency: 5, url: 'http://a.co', ab_options: '--foo').command }
+        it { should match '^ab' }
+        it("doesn't exit on socket receive") { should match '-r' }
+        it('should specify number of requests') { should match '-n 1000' }
+        it('should specify concurrency') { should match '-c 5' }
+        it('should set a dummy session id') { should match '-C "sessionid=fake"' }
+        it('should send extra options to ab') { should match '--foo' }
+        it('should pass the url') { should match '"http://a.co"$' }
+      end
+
+      it 'generate a valid ab command', integration: true do
+        command = described_class.new(requests: 1, concurrency: 1, url: 'http://www.google.com/').command
+        result = system(command)
+        result.should be_true
+      end
+
+      it 'executes a command on a worker' do
+        subject.stub(:command).and_return('ab')
+        worker = mock('Worker')
+        worker.should_receive(:run_command).with('ab')
+        subject.run(worker)
+      end
     end
   end
 end
