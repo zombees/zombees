c85b11dc5d9fa1798f17fd70941111b6e705de9c
Solomon White <rubysolo@gmail.com>
2013-05-10 08:27:39 -0600

    pass an adapter to the queen

--- spec/integration/running_a_server_swarm_spec.rb
+++ spec/integration/running_a_server_swarm_spec.rb
@@ -5,7 +5,7 @@ describe 'Running 3 servers', integration: true do
   let(:config) {{ provider: 'AWS', aws_access_key_id: 'foo', aws_secret_access_key: 'bar' }}
 
   it 'runs a command in parallel on all servers when they are ready' do
-    queen = Zombees::Queen.new(worker_count: 3, command: "ls", config: config)
+    queen = Zombees::Queen.new(worker_count: 3, command: Zombees::AbAdapter, config: config)
     queen.run
   end
 end
